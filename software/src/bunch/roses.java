f5457167cbc66abbf1d2d220f58f9de3a49c27e5
AdmiN <AdmiN@ITWS0049>
2024-08-29 11:15:37 +0530

    548 commit

--- software/src/bunch/roses.java
+++ software/src/bunch/roses.java
@@ -2,5 +2,11 @@ package bunch;
 
 public class roses {
 	//write your code here
+	public static void main(String args[])
+	{
+		System.out.println("Roses");
+		System.out.println("Roses are Red in color");
+	}
+	
 
 }
